130e02c0dd8486e93d6fbf3c31569626fe599478
Anand Rajaram <anandrajaram21@gmail.com>
2023-09-29 21:23:38 +0530

    basic scaffold

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
